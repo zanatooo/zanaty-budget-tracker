33b8b38504bb847a03306c00ef6ea3f52ab588d2
zanatooo <m8elzanaty@gmail.com>
2022-07-19 15:44:11 -0700

    finishing the idb.js file

--- public/js/idb.js
+++ public/js/idb.js
@@ -19,3 +19,75 @@ request.onerror = event => {
     // Create an objectStore for this database
     const objectStore = db.createObjectStore("name", { keyPath: "myKey" });
   };
+  request.onsuccess = function(event)
+{
+    db = event.target.result;
+    if(navigator.onLine)
+    {
+        uploadRecord();
+    }  
+};
+
+request.onerror = function(event) 
+{
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record)
+{
+    const transaction = db.transaction(['new_deposit'],'readwrite');
+
+    const depositObject = transaction.objectStore('new_deposit');
+
+    depositObject.add(record);
+}
+
+function uploadRecord()
+{
+    const transaction = db.transaction(['new_deposit'],'readwrite');
+
+    const depositObject = transaction.objectStore('new_deposit');
+
+    const getAll = depositObject.getAll();
+
+    getAll.onsuccess = function()
+    {
+        if(getAll.result.length)
+        {
+            fetch("/api/transaction", 
+            {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                  Accept: "application/json, text/plain, */*",
+                  "Content-Type": "application/json"
+                }
+            })
+            .then(response => 
+            {    
+                return response.json();
+            })
+            .then(data => 
+            {
+                if (data.message) 
+                {
+                    throw new Error(data);
+                }
+
+                const transaction = db.transaction(['new_deposit'],'readwrite');
+
+                const depositObject = transaction.objectStore('new_deposit');
+
+                depositObject.clear();
+
+                alert('All saved deposits have been submitted!');
+            })
+            .catch(err => 
+            {
+                console.log(err);
+            });
+        }
+    };
+}
+
+window.addEventListener('online', uploadRecord);

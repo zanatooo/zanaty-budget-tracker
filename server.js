a81e127cb0d37d366bfa2b0de335af67266bc588
zanatooo <m8elzanaty@gmail.com>
2022-07-25 17:28:29 -0700

    fixing database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+require("dotenv").config()
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
@@ -17,8 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
+  useNewUrlParser: true
 });
 
 // routes
